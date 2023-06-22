6ec56e6097381b6adefe052d194892eaa1bec8e9
demorunner <shownkenway@gmail.com>
2023-06-23 03:22:10 +0530

    added feature to populate the display by clicking number buttons

--- script.js
+++ script.js
@@ -34,5 +34,57 @@ function operate(n1, operator, n2) {
     }
 }
 
+textArea = document.getElementById("text-box");
+
+// document.getElementById("one").addEventListener("click", () => {
+//     document.getElementById("text-box").setAttribute('value', '1');
+//     textArea.value = textArea.value + "1";
+// });
+
+const btnOne = document.getElementById("one").addEventListener("click", () => {
+    textArea.value = textArea.value + '1';
+});
+
+const btnTwo = document.getElementById("two").addEventListener("click", () => {
+    textArea.value = textArea.value + '2';
+});
+
+const btnThree = document.getElementById("three").addEventListener("click", () => {
+    textArea.value = textArea.value + '3';
+});
+
+const btnFour = document.getElementById("four").addEventListener("click", () => {
+    textArea.value = textArea.value + '4';
+});
+
+const btnFive = document.getElementById("five").addEventListener("click", () => {
+    textArea.value = textArea.value + '5';
+});
+
+const btnSix = document.getElementById("six").addEventListener("click", () => {
+    textArea.value = textArea.value + '6';
+});
+
+const btnSeven = document.getElementById("seven").addEventListener("click", () => {
+    textArea.value = textArea.value + '7';
+});
+
+const btnEight = document.getElementById("eight").addEventListener("click", () => {
+    textArea.value = textArea.value + '8';
+});
+
+const btnNine = document.getElementById("nine").addEventListener("click", () => {
+    textArea.value = textArea.value + '9';
+});
+
+const btnZero = document.getElementById("zero").addEventListener("click", () => {
+    textArea.value = textArea.value + '0';
+});
+
+const btnClear = document.getElementById("clear").addEventListener("click", () => {
+    textArea.value = '';
+});
+
+
 console.log(operate(num1, operator, num2));
 
